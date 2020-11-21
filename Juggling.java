1f08b862095a83cf5fa3c6761c0f20ef4d200cc6
tarunguptaraja <the.tarun.gupta.raja@gmail.com>
2020-11-21 15:57:27 +0530

    initial commit

---
+++ Juggling.java
@@ -0,0 +1,24 @@
+package myAlgorithmsLibrary;
+
+public class Juggling {
+	
+	public static void leftRotate(int arr[], int d, int n) {
+        d = d % n; 
+        int i, j, k, temp; 
+        int g_c_d = Euclidean.gcd(d, n); 
+        for (i = 0; i < g_c_d; i++) {
+            temp = arr[i]; 
+            j = i;
+            while (true) { 
+                k = j + d; 
+                if (k >= n) 
+                    k = k - n; 
+                if (k == i) 
+                    break; 
+                arr[j] = arr[k]; 
+                j = k; 
+            } 
+            arr[j] = temp; 
+        } 
+    }
+}
